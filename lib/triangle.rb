073d51f048104096b4620e56a7a481b500a4fa5d
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-08-11 15:39:38 -0400

    finished the lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3
+
+  def initialize( side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
+  end
+
+  def kind
+    valid_triangle
+
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif 
+      side1 == side2 || side2 == side3 || side3 == side1
+      :isosceles
+    else
+    :scalene
+      end
+    end
+
+    def triangle_with_no_sides
+      [side1, side2, side3].all?(&:positive?)
+    end
+
+    def triangle_inequality
+      side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2
+    end
+    
+    def valid_triangle
+      raise TriangleError unless triangle_inequality && triangle_with_no_sides
+    end
+
+    class TriangleError < StandardError
+    end
+
 end
